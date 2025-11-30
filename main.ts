9f1d2ca8c1ecd7989dbce90315553aac9a0b4899
pabloPPR20 <pabl.pizarror@duocuc.cl>
2025-11-30 04:00:29 -0300

    feat: Configure Angular project for build and deployment
    Refactors project structure and configuration for a standard Angular CLI setup.
    Updates `angular.json`, `tsconfig.json`, and `tsconfig.app.json` to align with best practices and enable necessary features like zoneless change detection and HTTP client.
    Adjusts `index.html` to correctly point to the application root.
    Adds `vercel.json` for deployment configuration.
    Introduces `main.ts` for application bootstrapping.

---
+++ main.ts
@@ -0,0 +1,13 @@
+import '@angular/compiler';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideZonelessChangeDetection } from '@angular/core';
+
+import { AppComponent } from './src/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideZonelessChangeDetection(),
+    provideHttpClient()
+  ],
+}).catch((err) => console.error(err));
